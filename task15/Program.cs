0d328990321b519c9a7d6dee07fb2e31c2d01852
Andrey Shevchuk <Shevchuk339@mail.ru>
2023-01-30 01:01:57 +0500

    add task15

---
+++ task15/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите цифру от 1 до 7:   ");
+int a = int.Parse(Console.ReadLine()!);
+if (a >= 1 & a <= 7)
+    {
+        if(a == 6 || a == 7)
+            Console.WriteLine("Выходной!!!");
+        else
+            Console.WriteLine("Работаем, не отвлекаемся!");
+    }
+else
+    Console.WriteLine("Вы ввели что то не то...");
