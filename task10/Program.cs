d5ecdddfbc086824f7ac5d7888b262c56195cead
Andrey Shevchuk <Shevchuk339@mail.ru>
2023-01-30 00:09:12 +0500

    add task10

---
+++ task10/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int a = int.Parse(Console.ReadLine()!);
+int n1 = a / 100; 
+int n2 = a - (n1 * 100); 
+int n3 = n2 / 10;
+Console.Write($"Вторая цифра этого числа равна: {n3}");
