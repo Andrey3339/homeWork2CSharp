197ff3d9e0db1a4c02f29ff12b0937b90cdc1f93
Andrey Shevchuk <Shevchuk339@mail.ru>
2023-01-31 21:31:04 +0500

    add extraTask

---
+++ extraTask/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+Console.WriteLine("Введите последовательность натуральных чисел через Enter, в конце должен быть 0:  ");
+int a = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+int premax = 0;
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    max = a;
+    premax = b;
+}
+else if (a <= b)
+{
+    max = b;
+    premax = a;
+}
+while (a != 0)
+{
+    a = Convert.ToInt32(Console.ReadLine());
+    if (a > max & a > premax)
+    {
+        premax = max;
+        max = a;
+    }
+    if (a > max & a < premax)    
+    {
+        max = a;
+    }
+}
+Console.WriteLine("Предмаксимальное число введенной последовательности равно:  " + premax);
+Console.WriteLine("Максимальное число равно:  " + max);
